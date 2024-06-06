9d4c94e375196dba411e56be771a47be9a2a0c4d
zipang <christophe.desguez@gmail.com>
2024-06-07 00:51:36 +0200

    Change all methods to getters and  add a parent's getter

--- src/services/ResourceInfo.ts
+++ src/services/ResourceInfo.ts
@@ -7,20 +7,22 @@ export interface ResourceInfoOptions {
 	 */
 	rootDir?: string;
 	/**
-	 * The type of the ressource (file or directory)
+	 * The type of the resource (file or directory)
 	 * If not specified, the type will be inferred from the path
 	 */
 	type?: "file" | "dir";
 }
 
 /**
- * Describes a ressource of a File System
+ * Describes a resource of a File System
  * This minimal implementation will enforce our rules on resources paths :
  * - each path starts with a slash (/) meaning that it is the full path from the rootDir of the File System
  * - directories paths always end with a trailing slash (/)
  * - files paths do not end with a trailing slash (/) and MUST have an extension
  */
 export class ResourceInfo {
+	private _path: string;
+
 	constructor(rscPath?: string, options: ResourceInfoOptions = {}) {
 		if (!rscPath) {
 			throw new FileManagerError(400, "Empty path specified for resource");
@@ -33,56 +35,67 @@ export class ResourceInfo {
 			type = rscPath.match(/\.[a-zA-Z]+$/) ? "file" : "dir";
 		}
 
-		this.path = normalizePath(rscPath.replace(normalizePath(rootDir), ""), {
+		this._path = normalizePath(rscPath.replace(normalizePath(rootDir), ""), {
 			addLeadingSlash: true,
 			addTrailingSlash: type === "dir"
 		});
 	}
 
 	/**
-	 * The path to the ressource (file or directory)
+	 * The path to the resource (file or directory)
 	 * Each directory path ends with a trailing `/`.
 	 */
-	path: string;
+	get path(): string {
+		return this._path;
+	}
 
 	/**
 	 * @returns the name of the file or of the directory
 	 */
-	getName() {
-		const parts = this.path.split("/").filter(Boolean);
+	get name(): string {
+		const parts = this._path.split("/").filter(Boolean);
 		return parts.pop() || "<root>";
+	}
+
+	/**
+	 * @returns the path to the parent directory
+	 */
+	get parent(): string {
+		return this._path.substring(0, this._path.lastIndexOf("/"));
 	}
 
 	/**
 	 * @returns the extension of the file
 	 */
-	getExt() {
-		if (!this.isFile()) {
+	get ext(): string {
+		if (!this.isFile) {
 			throw new FileManagerError(400, "The resource is not a file");
 		}
-		return this.getName().split(".").pop() || "";
+		return this.name.split(".").pop() || "";
 	}
 
-	isText() {
-		// look at the mime type of the extension to know if is a known text format
-		return this.getExt().match(/txt|html|css|js|json|md|xml|yml|yaml/);
+	/**
+	 * @returns TRUE if the resource is a known text format
+	 */
+	get isText(): boolean {
+		return Boolean(this.ext.match(/txt|html|css|js|json|md|xml|yml|yaml/));
 	}
 
 	/**
 	 * @returns TRUE if the path is a directory, FALSE otherwise
 	 */
-	isDirectory() {
-		return this.path.endsWith("/");
+	get isDirectory(): boolean {
+		return this._path.endsWith("/");
 	}
 
 	/**
 	 * @returns TRUE if the path is a file, FALSE otherwise
 	 */
-	isFile() {
-		return !this.path.endsWith("/");
+	get isFile(): boolean {
+		return !this._path.endsWith("/");
 	}
 
-	toString() {
-		return this.path;
+	toString(): string {
+		return this._path;
 	}
 }
